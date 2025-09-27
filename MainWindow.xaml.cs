70cfba8dfeefbee4972ce1721a9d5c26c10ba3e6
KupriyanovIN <kupriyanovin@mer.ci.nsu.ru>
2025-09-27 12:35:48 +0700

    plusinkconvas

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -16,24 +16,104 @@ namespace WpfApp2
     /// </summary>
     public partial class MainWindow : Window
     {
+        private double brushSize = 5;
+        private Color brushColor = Colors.Red;
+        private InkCanvasEditingMode editingMode;
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void Blue_Click(object sender, RoutedEventArgs e)
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
         {
-            gr.Background = Brushes.Blue;
+            switch ((sender as FrameworkElement).Tag.ToString())
+            {
+                case "1":
+                    gr.Background = Brushes.Yellow;
+                    break;
+                case "2":
+                    gr.Background = Brushes.Red;
+                    break;
+                case "3":
+                    gr.Background = Brushes.Blue;
+                    break;
+                case "4":
+                    MessageBox.Show("Разработал студент вки");
+                    break;
+                case "5":
+                    Canv_1.EditingMode = editingMode;
+                    if (Canv_1.EditingMode == InkCanvasEditingMode.Ink) 
+                    {
+                        Canv_1.EditingMode = InkCanvasEditingMode.Ink;
+                        Canv_1.Visibility = Visibility.Visible;
+                        Canv_1.DefaultDrawingAttributes.Color = brushColor;
+                        Canv_1.DefaultDrawingAttributes.Width = brushSize;
+                        Canv_1.DefaultDrawingAttributes.Height = brushSize;
+                    }
+                    else if (Canv_1.EditingMode == InkCanvasEditingMode.EraseByPoint)
+                    {
+                        Canv_1.EditingMode = InkCanvasEditingMode.EraseByPoint;
+                        Canv_1.DefaultDrawingAttributes.Width = brushSize;
+                        Canv_1.DefaultDrawingAttributes.Height = brushSize;
+                    }
+                        break;
+            }
         }
 
-        private void Red_Click(object sender, RoutedEventArgs e)
+        private void Button_MouseEnter(object sender, MouseEventArgs e)
         {
-            gr.Background = Brushes.Red;
+            switch ((sender as FrameworkElement).Tag.ToString())
+            {
+                case "0":
+                    TextBlock_1.Text = "Эта кнопка открывае меню взаимодействия";
+                    break;
+                case "1":
+                    TextBlock_1.Text = "изменить фон на Жёлтый";
+                    break;
+                case "2":
+                    TextBlock_1.Text = "Изменить фон на красный";
+                    break;
+                case "3":
+                    TextBlock_1.Text = "Изменить фон на синий";
+                    break;
+                case "4":
+                    TextBlock_1.Text = "Узнать информацию о разработчике";
+                    break;
+            }
         }
 
-        private void Yallow_Click(object sender, RoutedEventArgs e)
+        private void SizeRadioButton_Checked(object sender, RoutedEventArgs e)
         {
-            gr.Background = Brushes.Yellow;
+            brushSize = double.Parse((sender as RadioButton).Content.ToString());
+        }
+
+        private void ColorRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            var radioButton = sender as RadioButton;
+            if (radioButton != null)
+            {
+                brushColor = radioButton.Content.ToString() switch
+                {
+                    "Red" => brushColor = Colors.Red,
+                    "Blue" => brushColor = Colors.Blue,
+                    "Green" => brushColor = Colors.Green,
+                    _ => brushColor = Colors.Red,
+                };
+            }
+        }
+
+        private void RezRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            var radioButton = sender as RadioButton;
+            if (radioButton != null)
+            {
+                var per = radioButton.Content.ToString() switch
+                {
+                    "Ink" => editingMode = InkCanvasEditingMode.Ink,
+                    "Remote" => editingMode = InkCanvasEditingMode.EraseByPoint,
+                    _ => editingMode = InkCanvasEditingMode.Ink,
+                };
+            }
         }
     }
 }
